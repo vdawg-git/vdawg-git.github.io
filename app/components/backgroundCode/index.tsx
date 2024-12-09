23f4c1fe718f95235bab9a4b450351c14190193d
VDawg <vdawg@tuta.io>
2024-12-09 23:00:52 +0100

    smaler code bg

--- app/components/backgroundCode/index.tsx
+++ app/components/backgroundCode/index.tsx
@@ -17,7 +17,7 @@ export function BackgroundCode() {
 				<pre>
 					<code
 						id="bg-code"
-						className="overflow-hidden absolute top-24 [text-shadow:0_0_18px_currentColor] left-0"
+						className="overflow-hidden text-[11px] absolute top-24 [text-shadow:0_0_8px_currentColor] left-0"
 					></code>
 				</pre>
 			</div>
