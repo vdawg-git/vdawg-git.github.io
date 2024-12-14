12fd9cb8622107b024071b0ef2aefa5985275541
VDawg <vdawg@tuta.io>
2024-12-14 16:33:15 +0100

    gh pages Lets see if it works

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@
  */
 const nextConfig = {
 	output: "export",
+	images: {
+		unoptimized: true,
+	},
+	assetPrefix: process.env.ASSET_PREFIX,
+	basePath: process.env.BASE_PATH,
 }
 
 export default nextConfig
